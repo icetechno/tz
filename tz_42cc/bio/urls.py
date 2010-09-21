29368ea4923abe87c773a313658bb52fa7bf1be1
Andrey M <icetechno@mail.ru>
2010-09-21 22:37:38 +0300

    ticket:1 created application for view person data

--- tz_42cc/bio/urls.py
+++ tz_42cc/bio/urls.py
@@ -1,3 +1,5 @@
-urlpatterns = patterns('mysite.polls.views',
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('tz_42cc.bio.views',
     (r'^$', 'index'),
 )
--- tz_42cc/bio/views.py
+++ tz_42cc/bio/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from tz_42cc.bio.models import Person
+
+def index(request):
+    persons_data = Person.objects.all()
+    return render_to_response('bio/index.html', {'persons_data': persons_data})
