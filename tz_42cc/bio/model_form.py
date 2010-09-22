72d03ef24a114c8f9f8dd4b7be109f96739feacd
Andrey M <icetechno@mail.ru>
2010-09-22 18:05:48 +0300

    ticket:6 done, works fine. uses djagno SelectDateWidget()

--- tz_42cc/bio/model_form.py
+++ tz_42cc/bio/model_form.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
 from tz_42cc.bio.models import Person
+from django import forms
+from django.forms.extras.widgets import SelectDateWidget
 
 # Create the form class.
-class PersonForm(ModelForm):
+class PersonForm(ModelForm):   
+    birthdate = forms.DateField(widget=SelectDateWidget())
+    
     class Meta:
         model = Person
+    
