d9eecaf101937f7a441dc28ccfa3181529f63ccd
Andrey M <icetechno@mail.ru>
2010-09-24 20:49:51 +0300

    ticket:1 use form

--- tz_42cc/bio/views.py
+++ tz_42cc/bio/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render_to_response
-from tz_42cc.bio.models import Person
+from models import Person
+from model_form import PersonDetail
 
 def index(request):
+    first_person = Person.objects.all()[0]  #first person in DB       
+    form = PersonDetail(instance = first_person)     
+    return render_to_response('bio/index.html', {'form': form})
+
+    
     persons_data = Person.objects.all()
     return render_to_response('bio/index.html', {'persons_data': persons_data})
 
