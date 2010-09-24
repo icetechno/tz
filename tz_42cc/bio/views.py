75ccad96d2e4b501594029b66dfc3a5b819cd2f9
Andrey M <icetechno@mail.ru>
2010-09-24 22:59:12 +0300

    ticket:1 added RequestContext

--- tz_42cc/bio/views.py
+++ tz_42cc/bio/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render_to_response
 from models import Person
 from model_form import PersonDetail
+from django.template import RequestContext
 
 def index(request):
     first_person = Person.objects.all()[0]  #first person in DB       
     form = PersonDetail(instance = first_person)     
-    return render_to_response('bio/index.html', {'form': form})
+    return render_to_response('bio/index.html', 
+                        {'form': form}, 
+                        context_instance = RequestContext(request))
 
 def start_page(request):
-    return render_to_response('links.html', {})
+    return render_to_response('links.html',
+                        {},
+                        context_instance = RequestContext(request))
