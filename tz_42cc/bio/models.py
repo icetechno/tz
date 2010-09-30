b54a8bd6729696ed4f25bb06e3688849cb223c32
Andrey M <icetechno@mail.ru>
2010-09-30 17:58:12 +0300

    ticket:1 pep8 checks

--- tz_42cc/bio/models.py
+++ tz_42cc/bio/models.py
@@ -2,23 +2,23 @@
 
 from django.db import models
 
+
 #That class represents a person
 class Person(models.Model):
-    name =      models.CharField(max_length = 255,
-                            blank = False,
-                            verbose_name = u'Имя:')  
-    surname =   models.CharField(max_length = 255,
-                            blank = False,
-                            verbose_name = u'Фамилия:')    
-    bio =       models.TextField(max_length = 255,
-                            blank = True,
-                            verbose_name = u'Биография:')  
-    contacts =  models.CharField(max_length = 255,
-                            blank = False,
-                            verbose_name = u'Контакты:')
-    birthdate =  models.DateField(verbose_name = u'Дата рождения:',
-                            blank = True)
- 
+    name = models.CharField(max_length=255,
+                            blank=False,
+                            verbose_name=u'Имя:')
+    surname = models.CharField(max_length=255,
+                            blank=False,
+                            verbose_name=u'Фамилия:')
+    bio = models.TextField(max_length=255,
+                            blank=True,
+                            verbose_name=u'Биография:')
+    contacts = models.CharField(max_length=255,
+                            blank=False,
+                            verbose_name=u'Контакты:')
+    birthdate = models.DateField(verbose_name=u'Дата рождения:',
+                            blank=True)
+
     def __unicode__(self):
         return u'%s %s' % (self.name, self.surname)
-         
--- tz_42cc/bio/tests.py
+++ tz_42cc/bio/tests.py
@@ -1,15 +1,16 @@
 from django.test import TestCase
 from models import Person
 
+
 #Ticket1
 class BioTest(TestCase):
-    fixtures = ['initial_data.json',]   #my fixtures
-            
+    fixtures = ['initial_data.json', ]   # my fixtures
+
     def simpleTest(self):
         response = self.client.get('/')
         # Check that the response is 200 OK.
         self.failUnlessEqual(response.status_code, 200)
         # find data pattern
-        self.failIfEqual(response.content.find('0974865577'), -1, 'data pattern not found in response')
-        
-
+        self.failIfEqual(response.content.find('0974865577'),
+                          -1,
+                         'data pattern not found in response')
