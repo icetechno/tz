71ea2c82cd29cdbba9c894b9beff728db882570f
Andrey M <icetechno@mail.ru>
2010-10-08 16:10:59 +0300

    ticket:14 form bug fixed

--- tz_42cc/site_media/js/edit_form.js
+++ tz_42cc/site_media/js/edit_form.js
@@ -1,6 +1,6 @@
 $(document).ready(function() { 
     var options = { 
-        target:        '#output1',   // target element(s) to be updated with server response 
+       // target:        '#output1',   // target element(s) to be updated with server response 
         beforeSubmit:  disableForm,  // pre-submit callback 
         success:       enableForm  // post-submit callback 
  
@@ -14,10 +14,17 @@ $(document).ready(function() {
         // $.ajax options can be used here too, for example: 
         //timeout:   3000 
     }; 
+    
+    // bind to the form's submit event 
+    $('#editForm').submit(function() { 
+        // inside event callbacks 'this' is the DOM element so we first 
+        // wrap it in a jQuery object and then invoke ajaxSubmit 
+        $(this).ajaxSubmit(options); 
  
-    // bind form using 'ajaxForm' 
-    $('#editForm').ajaxForm(options); 
-    enableForm();
+        // !!! Important !!! 
+        // always return false to prevent standard browser submit and page navigation 
+        return false; 
+    }); 
 }); 
 
 function disableElem(index) {
@@ -41,4 +48,5 @@ function enableForm(responseText, statusText, xhr, $form)  {
 	$('#editForm').find('select').each(enableElem);
 	$('#editForm').find('input').each(enableElem);
 	$('#editForm').find('textarea').each(enableElem);
+	$('#progress').hide();
 } 
